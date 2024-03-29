22dda95a3d98568d58166173815543fd81083707
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-03-29 15:21:43 +0530

    Update bypass_scrape.py

--- FZBypass/core/bypass_scrape.py
+++ FZBypass/core/bypass_scrape.py
@@ -158,19 +158,20 @@ async def toonworld4all(url: str):
     
 async def tamilmv(url):
     cget = create_scraper().request
-    resp = cget("GET", url, allow_redirects=False)
+    resp = cget("GET", url)
     soup = BeautifulSoup(resp.text, 'html.parser')
     mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
     tor = soup.select('a[data-fileext="torrent"]')
     parse_data = f"<b><u>{soup.title.string}</u></b>"
     for no, (t, m) in enumerate(zip(tor, mag), start=1):
         filename = sub(r"www\S+|\- |\.torrent", '', t.string)
+        magnet_link = m['href'].split('&')[0]
+        magnet_link_with_ql = "/ql" + magnet_link
         parse_data += f'''
         
 {no}. <code>{filename}</code>
-┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>'''
+┖ <b>Links :</b> <a href="https://t.me/share/url?url={magnet_link_with_ql}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>'''
     return parse_data
-
 
 async def tamilblasters(url):
     cget = create_scraper().request
