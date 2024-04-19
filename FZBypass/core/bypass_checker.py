7ebc86379f567b7623799f09a4391d49d762634c
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-04-19 23:56:48 +0530

    Update bypass_checker.py

--- FZBypass/core/bypass_checker.py
+++ FZBypass/core/bypass_checker.py
@@ -41,7 +41,7 @@ async def direct_link_checker(link, onlylink=False):
     
     elif bool(match(r"https?:\/\/ronylink\.\S+", link)):
         blink = await transcript(link, "https://go.ronylink.com/", "https://livejankari.com/", 9)
-    elif bool(match(r"https?:\/\/(gyanilinks|gtlinks)\.\S+", link)):
+    elif bool(match(r"https?:\/\/gtlinks\.\S+", link)):
         blink = await transcript(link, "https://go.bloggingaro.com/", "https://hipsonyc.com/", 8)
     elif bool(match(r"https?:\/\/.+\.tnshort\.\S+", link)):
         blink = await transcript(link, "https://go.tnshort.net/", "https://usanewstoday.club/", 9)
