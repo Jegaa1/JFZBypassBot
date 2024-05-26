2283a843a80185af1799e5174e8d0285d07c684c
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-05-26 22:47:30 +0530

    Update bypass_checker.py

--- FZBypass/core/bypass_checker.py
+++ FZBypass/core/bypass_checker.py
@@ -48,7 +48,7 @@ async def direct_link_checker(link, onlylink=False):
     elif bool(match(r"https?:\/\/mdisky\.\S+", link)):
         blink = await transcript(link, "https://go.bloggingaro.com/", "https://www.bloggingaro.com/", 6)
     elif bool(match(r"https?:\/\/kingurl\.\S+", link)):
-        blink = await transcript(link, "https://go.kingurl.in/", "https://a1.bankshiksha.in/", 6)
+        blink = await transcript(link, "https://go.kingurl.in/", "https://a1.bankshiksha.in/", 7)
     elif bool(match(r"https?:\/\/offerlinks\.\S+", link)):
         blink = await transcript(link, "https://first.offerlinks.in/", "https://offerpagehub.fun/", 6)
     elif bool(match(r"https?:\/\/runurl\.\S+", link)):
