aea47b8d7248edc546350a91e240e45e371e41af
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-06-15 14:44:08 +0530

    Update bypass_scrape.py

--- FZBypass/core/bypass_scrape.py
+++ FZBypass/core/bypass_scrape.py
@@ -169,11 +169,11 @@ async def tamilmv(url):
     parse_data = f"<b><u><code>{soup.title.string}</code></u></b>"
     for no, (t, m) in enumerate(zip(tor, mag), start=1):
         filename = sub(r"www\S+|\- |\.torrent", "", t.string)
-        parse_data += f"""
+        #parse_data += f"""
         
-    {no}. <code>{filename}</code>
-    ┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>
-    """
+    #{no}. <code>{filename}</code>
+    #┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>
+    #"""
     
     parse_data += f"""
     <code>{m['href'].split('&')[0]}</code>
